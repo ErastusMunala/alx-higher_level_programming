63e14bc5d96fb1a7cff0f356b41479f5f6714ae1
Erastus Munala <munalaerastus@gmail.com>
2023-03-30 23:32:04 +0300

    classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Square related class"""
+
+
+class Square:
+    """Defines a square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A square with size"""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes the data."""
+        self.__size = size
